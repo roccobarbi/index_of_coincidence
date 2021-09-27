b4dbfb83669dab2b5dec57ae8a894680fe5c4d7d
rocco barbini <roccobarbi@gmail.com>
2021-09-27 23:18:36 +0200

    MVP written

---
+++ calculate_ic.py
@@ -0,0 +1,22 @@
+import sys
+
+from letter_count.letter_counter import LetterCounter
+
+
+def main(args):
+    if len(args) < 1:
+        raise Exception("no arguments passed")
+    counter = LetterCounter()
+    text = ""
+    with open(args[0], "r") as infile:
+        text = infile.read()
+        counter.set_text(text)
+    count = counter.count()
+    ic = 0
+    for letter in counter.get_alphabet():
+        ic += count[letter]/len(text) * (count[letter] - 1)/(len(text) - 1)
+    print("Index of coincidence: %.2f %%" % (ic * 100))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
