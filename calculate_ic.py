6677ffd139835fdce0f3a8060ddf68e260c0dc45
rocco barbini <roccobarbi@gmail.com>
2021-09-29 21:52:08 +0200

    Most probable language now calculated

--- calculate_ic.py
+++ calculate_ic.py
@@ -1,5 +1,6 @@
 import sys
 
+from data_repository import ic_by_language
 from letter_count.letter_counter import LetterCounter
 
 
@@ -31,10 +32,24 @@ def parse_args(args):
     return config
 
 
+def estimate_language(ic):
+    probable_language = None
+    difference = None
+    for language in ic_by_language.index_of_coincidence_by_language:
+        if probable_language is None:
+            probable_language = language
+            difference = abs(ic - ic_by_language.index_of_coincidence_by_language[language])
+        else:
+            if abs(ic - ic_by_language.index_of_coincidence_by_language[language]) < difference:
+                probable_language = language
+                difference = abs(ic - ic_by_language.index_of_coincidence_by_language[language])
+    return probable_language, ic_by_language.index_of_coincidence_by_language[probable_language]
+
+
 def main(args):
     if len(args) < 1:
         raise Exception("no arguments passed")
-    config = parse_args()
+    config = parse_args(args)
     counter = LetterCounter()
     with open(config.infile_name, "r") as infile:
         text = infile.read()
@@ -45,7 +60,10 @@ def main(args):
         ic += count[letter]/len(text) * (count[letter] - 1)/(len(text) - 1)
     print("Index of coincidence: %.2f %%" % (ic * 100))
     if config.estimate_language:
-        pass
+        probable_language, probable_language_ic = estimate_language(ic)
+        print()
+        print("Most probable language: " + probable_language)
+        print("Typical index of coincidence of most probable language:: %.2f %%" % (probable_language_ic * 100))
 
 
 if __name__ == "__main__":
